f418453f209643204e6831e0ddb9527637f48284
luccahuguet <lucca.huguet2@gmail.com>
2024-05-05 12:10:28 -0300

    Global Styles

--- App.js
+++ App.js
@@ -6,25 +6,59 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
+import { GlobalStyles } from "./constants/styles";
+import { Ionicons } from "@expo/vector-icons";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}
+    >
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="time-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="list-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
+};
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-  const BottomTabs = createBottomTabNavigator();
-
-  const ExpensesOverview = () => {
-    return (
-      <BottomTabs.Navigator>
-        <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
-        <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
-      </BottomTabs.Navigator>
-    );
-  };
   return (
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{ headerShown: false }}
+          />
           <Stack.Screen name="ManageExpense" component={ManageExpense} />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#e4d9fd",
+    primary100: "#c9b3fc",
+    primary200: "#af8efb",
+    primary400: "#7d47fa",
+    primary500: "#642ffa",
+    primary700: "#4d23e6",
+    primary800: "#3d1da9",
+    accent500: "#f7bc0c",
+    error50: "#ffebee",
+    error500: "#f44336",
+    gray500: "#39324a",
+    gray700: "#1f1a2e",
+  },
+};
